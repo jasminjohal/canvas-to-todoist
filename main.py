ff6e78118542f4a437ba1a14a05f512706992077
jjrx <jjohal52@gmail.com>
2022-03-08 18:09:19 -0800

    modularized code

--- main.py
+++ main.py
@@ -6,41 +6,53 @@ import pandas as pd
 load_dotenv()
 API_URL = "https://canvas.oregonstate.edu/"
 API_KEY = os.environ.get("CANVAS_KEY")
-
 canvas = Canvas(API_URL, API_KEY)
-course = canvas.get_course(1849691)  # cs362?
-# course = canvas.get_course(1784199)  # cs325
-# course = canvas.get_course(1877222)  # cs361
-print(course.name)
 
-dates = []
-tasks = []
-times = []
 
-assignments = course.get_assignments()
+def get_course_assignments(course_ID):
+    course = canvas.get_course(course_ID)
+    assignments = course.get_assignments()
+    print(f'Processing tasks for {course.name}...')
+    return assignments
 
-for assignment in assignments:
-    tasks.append(assignment.name)
-    dates.append(assignment.due_at)
 
-syllabus_df = pd.DataFrame(
-    {'Tasks': tasks, 'Dates': dates})
+def transform_assignments_to_df(assignments):
+    dates = []
+    tasks = []
 
-df = syllabus_df.sort_values(by=['Dates'])
+    for assignment in assignments:
+        tasks.append(assignment.name)
+        dates.append(assignment.due_at)
 
-df = df.rename(columns={"Dates": "DATE", "Tasks": "CONTENT"})
+    df = pd.DataFrame({'Tasks': tasks, 'Dates': dates})
+    return df
 
-# requisite columns for Todoist import
-df['TYPE'] = "task"
-df['PRIORITY'] = 4
-df['INDENT'] = ""
-df['AUTHOR'] = ""
-df['RESPONSIBLE'] = ""
-df['DATE_LANG'] = "en"
-df['TIMEZONE'] = ""
 
-# reorder columns and output to csv
-df = df[['TYPE', 'CONTENT', 'PRIORITY', 'INDENT', 'AUTHOR',
-         'RESPONSIBLE', 'DATE', 'DATE_LANG', 'TIMEZONE']]
+def process_df_for_todoist(df):
+    df = df.sort_values(by=['Dates'])
+    df = df.rename(columns={"Dates": "DATE", "Tasks": "CONTENT"})
 
-df.to_csv('test.csv')
+    # requisite columns for Todoist import
+    df['TYPE'] = "task"
+    df['PRIORITY'] = 4
+    df['INDENT'] = ""
+    df['AUTHOR'] = ""
+    df['RESPONSIBLE'] = ""
+    df['DATE_LANG'] = "en"
+    df['TIMEZONE'] = ""
+
+    # reorder columns and output to csv
+    df = df[['TYPE', 'CONTENT', 'PRIORITY', 'INDENT', 'AUTHOR',
+            'RESPONSIBLE', 'DATE', 'DATE_LANG', 'TIMEZONE']]
+
+    return df
+
+
+if __name__ == "__main__":
+    courses = {'cs362': 1849691, 'cs325': 1784199, 'cs361': 1877222}
+    course_ID = courses['cs361']
+    assignments = get_course_assignments(course_ID)
+    df = transform_assignments_to_df(assignments)
+    df = process_df_for_todoist(df)
+    df.to_csv(f'test.csv')
+    print('Processing complete!')
